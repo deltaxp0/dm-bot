1380dbad31d37a59b7504102d1b7ce84056ac499
deltaxp0 <jekerdev@gmail.com>
2024-08-17 18:57:34 +0300

    Added pinging functionality in-between sectors.

--- dm-bot.py
+++ dm-bot.py
@@ -1,6 +1,5 @@
 import discord
 import random
-from discord.ext import commands
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -9,9 +8,13 @@ client = discord.Client(intents=intents)
 
 players = {}
 stats = {}
+ids = {}
+
+sectors = {}
 
 registered_messages = []
 
+#Delta: Added player class. Should be extensible enough.
 class Player:
     def __init__(self, name, strength, speed, defence):
         self.name = name
@@ -22,15 +25,19 @@ class Player:
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    channel = client.get_channel('CHANNEL_ID') 
+    channel = client.get_channel(1272746089555955758)
+    sectors['test'] = client.get_channel(1274370660226957342)
+    sectors['test-2'] = client.get_channel(1274388708073668720)
+    sectors['test-3'] = client.get_channel(1274388788092731434)
+
     register_message = await channel.send("Please react to this message to be registed as a player!")
-    await register_message.add_reaction('✅')
+    await register_message.add_reaction('💀')
     registered_messages.append(register_message)
 
 @client.event
 async def on_message(message):
     if message.content.startswith('!list'):
-        dm_role = discord.utils.get(message.author.roles, name="DM")
+        dm_role = discord.utils.get(message.author.roles, name="D&D Staff")
         if dm_role is None:
             return
         grouped_data = {}
@@ -40,45 +47,30 @@ async def on_message(message):
             grouped_data[value].append(key)
         #await message.channel.send(str(players))
 
-        embed = discord.Embed(
-            title="Grouped Data",
-            description="Here are the keys grouped by their values:",
-            color=discord.Color.blue()
-        )
-
         embed=discord.Embed(title="List")
         for value, keys in grouped_data.items():
-        # Join the keys into a single string, separated by commas
+        # Join the keys into a single string, separated by a new line
             keys_str = "\n".join(keys)
         # Add a field to the embed
             embed.add_field(name=value, value=keys_str, inline=False)
         await message.channel.send(embed=embed)
     
     if message.content.startswith('!move'):
-        dm_role = discord.utils.get(message.author.roles, name="DM")
+        dm_role = discord.utils.get(message.author.roles, name="D&D Staff")
         if dm_role is None:
             return
         msg = message.content.split()
         if len(msg) < 3 or len(msg) > 3:
            await message.channel.send("Incorrect usage! Should be: !move [player] [sector]")
-           return 
-        elif len(msg) == 3: #msg[1] = user, msg[2] = sector
+           return
+        elif len(msg) == 3: #msg[1] = user, msg[2] = sector | Delta: Useless elif statement, lol. 
             if msg[1] in players:
                 players[msg[1]] = msg[2]
+                await sectors[msg[2]].send(f"<@{ids[msg[1]]}> welcome to {msg[2]}!")
             else:
                 await message.channel.send("No such registered player found!")
                 print(msg[1])
                 return
-    if message.content.startswith('!roll'):
-        msg = message.content.split()
-        if len(msg) < 2 or len(msg) > 2 or int(msg[1]) not in [4,6,8,10,12,20]:
-            await message.channel.send("Incorrect usage! Should be: !roll [4,6,8,10,12,20]")
-            return
-        else:
-            sides = int(msg[1])
-            await message.channel.send(str(random.randint(1, sides)))
-
-
     if message.content.startswith('!register_stats'):
         dm_role = discord.utils.get(message.author.roles, name="D&D Staff")
         if dm_role is None:
@@ -115,10 +107,22 @@ async def on_message(message):
        except KeyError:
            await message.channel.send("No such player found!")
            return
-            
+    
+    if message.content.startswith('!roll'):
+        msg = message.content.split()
+        if len(msg) < 2 or len(msg) > 2 or int(msg[1]) not in [4,6,8,10,12,20]:
+            await message.channel.send("Incorrect usage! Should be: !roll [4,6,8,10,12,20]")
+            return
+        else:
+            sides = int(msg[1])
+            await message.channel.send(str(random.randint(1, sides)))
+           
 @client.event
 async def on_reaction_add(reaction, user):
-    if reaction.message == registered_messages[0]:
-        players[user.name] = 'Default-Sector'
+    global other_channel
+    if reaction.message == registered_messages[0] and user.name not in players:
+        players[user.name] = 'test'
+        ids[user.name] = user.id
+        await sectors["test"].send(f"<@{user.id}> welcome to test!")
 
 client.run('TOKEN')
