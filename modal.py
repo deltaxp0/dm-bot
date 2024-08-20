b895ccd3b1fe4d788325a3864e573823e582e638
deltaxp0 <jekerdev@gmail.com>
2024-08-21 00:27:46 +0300

    Added age and an embed.

--- modal.py
+++ modal.py
@@ -5,6 +5,7 @@ options = [discord.SelectOption(label="None"), discord.SelectOption(label="Mediu
 class RegisterModal(discord.ui.Modal, title="Register"):
     user_nickname = discord.ui.TextInput(label="Please enter your prefered nickname.", required=True, max_length=50, style=discord.TextStyle.short)
     user_pronouns = discord.ui.TextInput(label="Please enter your prefered pronouns.", required=True, max_length=50, style=discord.TextStyle.short)
+    user_age = discord.ui.TextInput(label="Please enter your age", required=True, max_length=3, style=discord.TextStyle.short)
 
     registered_users = []
 
@@ -19,11 +20,20 @@ class RegisterModal(discord.ui.Modal, title="Register"):
         async def select_callback(interaction):
             if interaction.user.id not in self.registered_users:
                 log = discord.utils.get(interaction.guild.channels, name="log")
-                await log.send(f"User {interaction.user.name} registered.\nID: {interaction.user.id}, \nNickname: {self.user_nickname}, \nPronouns: {self.user_pronouns}, \nExperience: {select.values[0]}")
+                embed = discord.Embed(title=f"Register")
+                embed.add_field(name="Name", value=interaction.user.name)
+                embed.add_field(name="ID", value=interaction.user.id)
+                embed.add_field(name="Nickname", value=self.user_nickname)
+                embed.add_field(name="Pronouns", value=self.user_pronouns)
+                embed.add_field(name="Age", value=self.user_age)
+                embed.add_field(name="Experience", value=select.values[0])
+                await log.send(embed=embed)
                 await interaction.response.send_message(f"Thank you for registering!", ephemeral=True)
                 self.registered_users.append(interaction.user.id)
             else:
                 await interaction.response.send_message(f"You have already registered!", ephemeral=True)
+
+        select.callback = select_callback
 
         select.callback = select_callback
     
